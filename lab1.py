dfb5a8dfaa4ea4b5c555c6230f99a9ee31727c77
pakra1 <p.pakravan@mail.utoronto.ca>
2015-10-01 12:20:00 -0400

    Lab Exercise 1
    Completed

--- lab1.py
+++ lab1.py
@@ -2,8 +2,8 @@
 
 """ Graded Lab #1 for Inf1340, Fall 2015 """
 
-__author__ = 'Susan Sim'
-__email__ = "ses@drsusansim.org"
+__author__ = 'Paniz Pakravan'
+__email__ = "p.pakravan@mail.utoronto.ca"
 __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
@@ -17,4 +17,23 @@ def vowel_or_consonant():
     "sometimes a vowel, sometimes a consonant". Otherwise your program should
     display a message indicating that the letter is a "consonant".
     """
+
+    respond = raw_input("what is your letter?")
+    if len(respond) == 1:
+        if respond == "a":
+            print("vowel")
+        elif respond == "e":
+            print("vowel")
+        elif respond == "i":
+            print("vowel")
+        elif respond == "o":
+            print("vowel")
+        elif respond == "u":
+            print ("vowel")
+        elif respond == "y":
+            print("sometimes a vowel, sometimes a consonant")
+        else:
+            print("consonant")
+    else:
+        print("error")
 
