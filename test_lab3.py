0833e7e3af2f89e84e64d8a1dacf1c3916491ede
pakra1 <p.pakravan@mail.utoronto.ca>
2015-10-29 12:07:56 -0400

    Completed Lab 3

--- test_lab3.py
+++ test_lab3.py
@@ -2,10 +2,10 @@
 
 """ Module to test lab3.py """
 
-__author__ = 'Susan Sim'
-__email__ = "ses@drsusansim.org"
-__copyright__ = "2015 Susan Sim"
-__license__ = "MIT License"
+__author__ = 'Paniz Pakravan'
+__email__ = "p.pakravan@mail.utoronto.ca"
+__copyright__ = "2015 Paniz Pakravan"
+__date__ = "29 October 2015"
 
 from lab3 import days_in_month
 
@@ -21,11 +21,31 @@ def test_months_with_31():
     for item in MONTHS_WITH_31:
         assert days_in_month(item) == 31
 
+def test_months_with_less():
+    for item in MONTHS_WITH_30 or MONTHS_WITH_28_or_29:
+        assert True
+
+    try:
+        days_in_month(item) == 30 or 29 or 28
+    except ValueError:
+        assert True
+
+def lower_days_in_month():
+    for item in MONTHS_WITH_31 or MONTHS_WITH_30 or MONTHS_WITH_28_or_29:
+        item = item.lower()
+        assert lower_days_in_month(item) == 31
+        assert lower_days_in_month(item) == 30
+        assert lower_days_in_month(item) == "28 or 29"
+
+def days_in_month_exception():
+    try:
+        item = MONTHS_WITH_28_or_29 or MONTHS_WITH_30 or MONTHS_WITH_31
+    except ValueError:
+        Print ("Oops")
+
 # Write a test function for the months with 30 days
 
-
 # Write a test function for the months with 28 or 29 days
-
 
 # Write a test function for months that are not capitalized properly
 # Hint: use the lower() string method
